ce3efb9fe4ad4846adece06fba0af7797da40575
Matthew Wesley-James <mwesleyjames@gmail.com>
2020-01-31 01:00:27 -0500

    edge detect update

--- ShapeDetection.py
+++ ShapeDetection.py
@@ -5,6 +5,7 @@
 #
 #   Shape Detection with Fourier Descriptors
 
+import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
@@ -16,11 +17,19 @@ def importImg(file):
 
 def showImg(img):
     plt.title('OG img')
-    plt.imshow(img, cmap='gray', interpolation='bicubic') # nearest, bilinear, bicubic
+    plt.imshow(img, cmap='gray', interpolation='nearest') # nearest, bilinear, bicubic
 
 def showCircle(xx, yy, radius):
     circle = plt.Circle((xx, yy), radius, color='r', fill=False)
     plt.gcf().gca().add_artist(circle)
+
+def hist(img):
+    hist = []
+    print()
+    for i in range(0,256):
+        hist.append(len(np.where(img == i)[0]))
+        if hist[i] > 0:
+            print(i, ':', hist[i])
 
 
 
@@ -35,9 +44,20 @@ f = 'shapes.jpg'
 img = importImg(f)
 # """
 
+mask = cv.Canny(img, 200, 0)
+hist(mask)
 
+# while (len(np.where(img == 255)[0])):
+#     shape = []
+#     y = list(np.where(img == 255))[0]
+#     print(len(y))
 
 # """
+plt.subplot(1,2,1)
 showImg(img)
+
+plt.subplot(1,2,2)
+showImg(mask)
+
 
 plt.show()
